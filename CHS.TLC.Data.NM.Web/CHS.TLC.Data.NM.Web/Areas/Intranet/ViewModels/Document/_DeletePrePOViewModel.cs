f6d8058a3d0f7008fdd897d0999d7304b4cade48
Carlos <alcros.12@gmail.com>
2019-03-15 05:01:16 -0500

    be json result

---
+++ CHS.TLC.Data.NM.Web/CHS.TLC.Data.NM.Web/Areas/Intranet/ViewModels/Document/_DeletePrePOViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CHS.TLC.Data.NM.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHS.TLC.Data.NM.Web.Areas.Intranet.ViewModels.Document
+{
+    public class _DeletePrePOViewModel
+    {
+        public Int32 PrePurcherseOrderId { get; set; }
+        public String Code { get; set; }
+        public void Fill(CargarDatosContext c, Int32 prePurcherseOrderId)
+        {
+            this.PrePurcherseOrderId = prePurcherseOrderId;
+            this.Code = c.context.PrePurcherseOrder.FirstOrDefault( x => x.PrePurcherseOrderId == this.PrePurcherseOrderId).Code;
+        }
+    }
+}
