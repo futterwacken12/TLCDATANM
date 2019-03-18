846054496a050fc99b766ce8e446148495cf7097
Carlos <alcros.12@gmail.com>
2019-03-18 01:05:08 -0500

    be

---
+++ CHS.TLC.Data.NM.Web/CHS.TLC.Data.NM.Web/Areas/Intranet/Models/BE/Json/LstPurcherseOrderInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHS.TLC.Data.NM.Web.Areas.Intranet.Models.BE.Json
+{
+    public class LstPurcherseOrderInfo
+    {
+        public List<PurcherseOrderInfo> lstPurcharseOrder = new List<PurcherseOrderInfo>();
+        public List<Int32> lstProductId = new List<Int32>();
+    }
+}
