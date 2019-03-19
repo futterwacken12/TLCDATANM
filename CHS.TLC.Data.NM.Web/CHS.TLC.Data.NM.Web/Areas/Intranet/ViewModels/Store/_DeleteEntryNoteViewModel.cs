c14f23a4da512caf24a3ec473da2ad2f068364ed
Carlos <alcros.12@gmail.com>
2019-03-19 09:37:30 -0500

    delete entry note

---
+++ CHS.TLC.Data.NM.Web/CHS.TLC.Data.NM.Web/Areas/Intranet/ViewModels/Store/_DeleteEntryNoteViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CHS.TLC.Data.NM.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHS.TLC.Data.NM.Web.Areas.Intranet.ViewModels.Store
+{
+    public class _DeleteEntryNoteViewModel
+    {
+        public Int32 EntryNoteId { get; set; }
+        public String Code { get; set; }
+        public void Fill(CargarDatosContext c, Int32 entryNoteId)
+        {
+            this.EntryNoteId = entryNoteId;
+            this.Code = c.context.EntryNote.FirstOrDefault( x => x.EntryNoteId == this.EntryNoteId).Code;
+        }
+    }
+}
